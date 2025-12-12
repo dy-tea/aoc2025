67092741980716c168ab4f5b47637f2ddd577274
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-12-12 14:55:00 +0000

    chore: v fmt -w .

--- 12.v
+++ 12.v
@@ -8,15 +8,15 @@ sizes := arrays.chunk(lines[1..29].filter(|line| line != '' && !line.contains(':
 // part 1
 mut count := 0
 for line in areas {
-  sarea, svals := line.split_once(': ')?
-  w, h := sarea.split_once('x')?
-  vals := svals.split(' ')
-  mut size := 0
-  for i, n in vals {
-    size += sizes[i] * n.int()
-  }
-  if size <= w.int() * h.int() {
-    count++
-  }
+	sarea, svals := line.split_once(': ')?
+	w, h := sarea.split_once('x')?
+	vals := svals.split(' ')
+	mut size := 0
+	for i, n in vals {
+		size += sizes[i] * n.int()
+	}
+	if size <= w.int() * h.int() {
+		count++
+	}
 }
 println(if areas.len < 10 { count - 1 } else { count })
